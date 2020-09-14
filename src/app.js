83099dd2e04dd7788a910ee539288d9fc6649d6c
dark <473380917@qq.com>
2020-09-14 17:36:26 +0800

    完成：按钮组合

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './Icon.vue'
+import ButtonGroup from './button-group.vue'
+
 Vue.component('w-button', Button)
 Vue.component('w-icon', Icon)
+Vue.component('w-button-group', ButtonGroup)
 new Vue({
     data(){
         return{
---
+++ src/button-group.vue
@@ -0,0 +1,36 @@
+<template>
+   <div class="button-group">
+       <slot></slot>
+   </div>
+</template>
+
+<script lang='ts'>
+  export default {
+  };
+
+</script>
+
+<style lang="scss" scoped>
+    .button-group{
+        vertical-align: middle;
+        display: inline-flex;
+        > .w-button {
+            border-radius: 0;
+
+            &:not(:first-child) {
+                margin-left: -1px;
+            }
+            &:first-child{
+                border-bottom-left-radius: var(--border-radius);
+                border-top-left-radius: var(--border-radius);
+            }
+            &:last-child{
+                border-bottom-right-radius: var(--border-radius);
+                border-top-right-radius: var(--border-radius);
+            }
+        }
+
+
+
+    }
+</style>
