99f55079143940e9a46786af11488a2699dee49f
dark <473380917@qq.com>
2020-09-14 15:41:03 +0800

    添加Icon

---
+++ src/Icon.vue
@@ -0,0 +1,24 @@
+<template>
+    <svg class="w-icon">
+        <use :xlink:href="`#i-${name}`"></use>
+    </svg>
+</template>
+
+<script lang='ts'>
+  export default {
+
+    props:{
+      name:{
+        type:String
+      }
+    }
+  };
+
+</script>
+
+<style lang="scss" scoped>
+    .w-icon{
+        width: 1em;
+        height: 1em;
+    }
+</style>
