c8e3329a7cb4462ba0f6dcbbddbbee53178f12e4
dark <473380917@qq.com>
2020-09-14 17:51:28 +0800

    完成：按钮组

--- src/button-group.vue
+++ src/button-group.vue
@@ -6,6 +6,15 @@
 
 <script lang='ts'>
   export default {
+    mounted(){
+      console.log(this.$el.children)
+      for(let node of this.$el.children){
+        const name  = node.nodeName.toLowerCase()
+        if(name!=='button'){
+          console.warn(`g-button-group 的子元素应该全是 g-button，但是你写的是 ${name}`)
+        }
+      }
+    }
   };
 
 </script>
@@ -32,8 +41,5 @@
                 border-top-right-radius: var(--border-radius);
             }
         }
-
-
-
     }
 </style>
