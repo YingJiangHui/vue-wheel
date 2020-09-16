6759a4373ed6be9937c3cbcc8f00fea1d743a9f1
dark <473380917@qq.com>
2020-09-16 10:01:35 +0800

    完成：input组件样式

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './Icon.vue'
 import ButtonGroup from './button-group.vue'
-
+import Input from './input.vue'
 Vue.component('w-button', Button)
 Vue.component('w-icon', Icon)
 Vue.component('w-button-group', ButtonGroup)
+Vue.component('w-input', Input)
+
 new Vue({
     data(){
         return{
---
+++ src/input.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="wrapper">
+        <input type="text" :readonly="readonly" :disabled="disabled" :value="value" :class="{error}">
+        <template v-if="error">
+            <w-icon name="error" class="icon-error"></w-icon>
+            <span class="error-message">{{error}}</span>
+        </template>
+    </div>
+</template>
+
+<script lang='ts'>
+  export default {
+    name: 'WInput',
+    props: {
+      value: {
+        type: String
+      },
+      error: {
+        type: String
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      readonly: {
+        type: Boolean,
+        default: false
+      }
+    }
+  };
+
+</script>
+
+<style lang="scss" scoped>
+    $font-size: 14px;
+    $border-color: #999;
+    $border-radius: 4px;
+    $border-hover-color: #666;
+    $shadow-color: rgba(0, 0, 0, 0.25);
+    $error-color: #F1453D;
+    $shadow-error-color: rgba(255, 0, 0, .2);
+    $disable-color: #bbb;
+    .wrapper {
+        font-size: $font-size;
+
+        > input {border-radius: $border-radius;height: 32px;font-size: inherit;padding: 0 8px;
+            outline: none;border: 1px solid $border-color;transition: box-shadow .25s;
+
+            &:hover {border-color: $border-hover-color;}
+
+            &:focus {box-shadow: 0 0 0 4px $shadow-color;}
+
+            &[disabled], &[readonly] {color: $disable-color;border-color: $disable-color;cursor: not-allowed;
+
+                &:focus {box-shadow: none;}
+            }
+
+            &.error {border-color: $error-color;
+
+                &:focus {box-shadow: 0 0 0 4px $shadow-error-color;}
+            }
+        }
+
+        > .error-message {color: $error-color;}
+
+        > .icon-error {fill: $error-color;}
+
+    }
+</style>
