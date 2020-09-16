4968ec8ef8ae7841ca10233de39be94de8cd3848
dark <473380917@qq.com>
2020-09-16 16:28:02 +0800

    初步完成：栅格系统

---
+++ src/col.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="col" :class="[span && `col-${span}`,offset && `offset-${offset}`]">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang='ts'>
+  export default {
+    name: 'WCol',
+    props: {
+      span: {
+        type: [String, Number]
+      },
+      offset: {
+        type: [String, Number]
+      }
+    }
+  };
+
+</script>
+
+<style lang="scss" scoped>
+    .col {
+        width: 100%;
+        height: 100px;
+        border: 1px solid rebeccapurple;
+
+        $class-prefix: 'col-';
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                width: ($n/24)*100%;
+            }
+        }
+
+    }
+
+    $class-prefix: 'offset-';
+    @for $n from 1 through 24{
+        .#{$class-prefix}#{$n}{
+            margin-left: ($n/24)*100%;
+        }
+    }
+
+</style>
---
+++ src/row.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang='ts'>
+  export default {
+    name: 'WRow'
+  };
+
+</script>
+
+<style lang="scss" scoped>
+.row{
+    width: 100%;
+    display: flex;
+}
+</style>
