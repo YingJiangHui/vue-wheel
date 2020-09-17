ee4f73d74a6bc571b9d6657ee737f3a653d1a887
dark <473380917@qq.com>
2020-09-17 15:12:45 +0800

    完成：保证同一时刻只可以有一个toast组件在页面上

--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,24 @@
 import Toast from './toast'
 
+let currentToast
 export default {
     install(Vue, options) {
-        Vue.prototype.$toast = function (message,toastOptions) {
-            const Constructor = Vue.extend(Toast)
-            const toast = new Constructor({
-                propsData:toastOptions
-            })
-            toast.$slots.default = [message]
-            toast.$mount()
-            document.body.appendChild(toast.$el)
+        Vue.prototype.$toast = function (message, toastOptions) {
+            if (currentToast)
+                currentToast.close()
+
+            currentToast = xxx({Vue, message, propsData: toastOptions})
         }
     }
+}
+
+function xxx({Vue, message, propsData}) {
+    const Constructor = Vue.extend(Toast)
+    const toast = new Constructor({
+        propsData
+    })
+    toast.$slots.default = [message]
+    toast.$mount()
+    document.body.appendChild(toast.$el)
+    return toast
 }
