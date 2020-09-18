8bf69fbd6deafed03ea5f3fb749052d8acf52cac
dark <473380917@qq.com>
2020-09-18 15:36:43 +0800

    完成：完成部分测试用例

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="eagle-tabs-item" @click="onSelect" :class="classes">
+    <div class="eagle-tabs-item" @click="onSelect" :class="classes" :data-name="name">
         <slot></slot>
     </div>
 </template>
@@ -9,8 +9,8 @@
     name: 'EagleTabsItem',
     inject: ['eventBus'],
     props: {
-      disabled:{
-        type:Boolean,
+      disabled: {
+        type: Boolean,
         default: false
       },
       name: [String, Number],
@@ -22,41 +22,46 @@
       };
     },
     computed: {
-      classes () {
+      classes() {
         return {
           active: this.active,
           disabled: this.disabled
-        }
+        };
       }
     },
     methods: {
       onSelect() {
-        if(this.disabled) return;
-        this.eventBus.$emit('update:selected', this.name,this);
+        if (this.disabled) return;
+        this.eventBus&&
+        this.eventBus.$emit('update:selected', this.name, this);
+        this.$emit('click', this)
       }
     },
     created() {
-      this.eventBus.$on('update:selected', (name) => {
-        this.active = this.name === name;
-      });
+      if (this.eventBus)
+        this.eventBus.$on('update:selected', (name) => {
+          this.active = this.name === name;
+        });
     }
   };
 
 </script>
 
 <style lang="scss" scoped>
-    .eagle-tabs-item{
-        &{
+    .eagle-tabs-item {
+        & {
             display: flex;
             align-items: center;
             padding: 0 1em;
             cursor: pointer;
             height: 100%;
         }
+
         &.active {
             color: dodgerblue;
         }
-        &.disabled{
+
+        &.disabled {
             color: #aaa;
         }
     }
---
+++ test/tabs-item.test.js
@@ -0,0 +1,35 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import TabsItem from '../src/tabs-item'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Button', () => {
+    it('存在.', () => {
+        expect(TabsItem).to.be.ok
+    })
+    it('接收 disabled ', () => {
+        const Constructor = Vue.extend(TabsItem)
+        const vm = new Constructor({
+            propsData:{
+                disabled:true
+            }
+        }).$mount()
+        expect(vm.$el.classList.contains('disabled')).to.be.true
+        const callback = sinon.fake()
+        vm.$on('click',callback)
+        vm.$el.click()
+        expect(callback).to.have.not.been.called
+        vm.$destroy()
+    })
+    it('接受 name 属性', () => {
+        const Constructor = Vue.extend(TabsItem)
+        const vm = new Constructor({
+            propsData: {
+                name: 'xxx',
+            }
+        }).$mount()
+        expect(vm.$el.getAttribute('data-name')).to.eq('xxx')
+    })
+})
